ba68c7deae6c5c05460087af2b75e084f8cc5ae5
Kepler Fontenele <keplerf@criticalmass.com>
2023-03-23 10:58:47 -0700

    init

--- src/shaders/test/fragment.glsl
+++ src/shaders/test/fragment.glsl
@@ -63,7 +63,26 @@ void main()
 
 
       // Pattern 16
-    float strenght = abs(vUv.x - 0.5);
+    // float strenght = abs(vUv.x - 0.5);
+
+       // Pattern 17
+    // float strenght = min(abs(vUv.x - 0.5) , abs(vUv.y - 0.5));
+
+       // Pattern 18
+    // float strenght = max(abs(vUv.x - 0.5) , abs(vUv.y - 0.5));
+
+       // Pattern 19
+    // float square1 = 1.0 - step(0.4, max(abs(vUv.x - 0.5) , abs(vUv.y - 0.5)));
+    // float square2 = step(0.3, max(abs(vUv.x - 0.5) , abs(vUv.y - 0.5)));
+
+
+    //  float strenght = square1 * square2;
+
+    // Pattern 21
+    // float strenght = floor(vUv.x * 10.0) / 10.0;
+
+    // Pattern 22
+    // float strenght = floor(vUv.x * 10.0) / 10.0 * floor(vUv.y * 10.0) / 10.0;
   
     gl_FragColor = vec4(strenght ,strenght ,strenght, 1.0);
 }
